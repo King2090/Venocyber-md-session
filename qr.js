0134a0fd374237d3179ff07183cdc9e9c6ceb992
King2090 <168007603+King2090@users.noreply.github.com>
2024-06-09 04:41:19 +0000

    Update qr.js

--- qr.js
+++ qr.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Venocyber_Tech,
+	default: Bryant_Tech,
 	useMultiFileAuthState,
 	jidNormalizedUser,
 	Browsers,
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function VENOCYBER_MD_QR_CODE() {
+	async function MADARA_MD_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Venocyber_Tech = Venocyber_Tech({
+			let Qr_Code_By_Bryant_Tech = Bryant_Tech({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Venocyber_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Venocyber_Techr.ev.on("connection.update", async (s) => {
+			Qr_Code_By_Bryant_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Bryant_Techr.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -63,7 +63,7 @@ router.get('/', async (req, res) => {
 *_Made With 🤍_*
 ______________________________________
 *_Pair Code Connected by Venocyber Tech_*
-*_Made With 🤍_*
+*_Made With _*
 ______________________________________
 ╔════◇
 ║ *『 WOW YOU CHOOSEN VENOCYBER-MD 』*
@@ -71,26 +71,26 @@ ______________________________________
 ╚══════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@JASTINMTEWA-vn9pl_
-║❒ *Owner:* _https://wa.me/message/A4QG2JZKBXFTN1_
-║❒ *Repo:* _https://github.com/Kingjux/venocyber-md_
-║❒ *WaGroup:* _https://chat.whatsapp.com/HSln3blDuuuKvC8njxyCCN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l_
-║❒ *Plugins:* _https://github.com/Kingjux/venocyber-md-plugins_
+║❒ *Ytube:* _youtube.com/@Bryant_tech.1_
+║❒ *Owner:* _https://wa.me/233538230444
+║❒ *Repo:* _https://github.com/BryantXtech/Madara-md_
+║❒ *WaGroup:* _https://chat.whatsapp.com/DOko0OMbzD3DPZmIADnT95_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VacpEdXIt5rqKLB9nC1L_
+║❒ *OWNER:* _𝚩𝚪𝐘𝚫𝚴𝚻 𝚻𝚵𝐂𝚮_
 ╚══════════════════════╝ 
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_Venocyber_Tech.sendMessage(Qr_Code_By_Venocyber_Tech.user.id,{text:Venocyber_MD_TEXT},{quoted:session})
+	 await Qr_Code_By_Bryant_Tech.sendMessage(Qr_Code_By_Bryant_Tech.user.id,{text:MADARA_MD_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Venocyber_Tech.ws.close();
+					await Qr_Code_By_Bryant_Tech.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
-					VENOCYBER_MD_QR_CODE();
+					MADARA_MD_QR_CODE();
 				}
 			});
 		} catch (err) {
@@ -103,6 +103,6 @@ _Don't Forget To Give Star To My Repo_`
 			await removeFile("temp/" + id);
 		}
 	}
-	return await VENOCYBER_MD_QR_CODE()
+	return await MADARA_MD_QR_CODE()
 });
 module.exports = router
